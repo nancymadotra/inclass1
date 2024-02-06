fd531d9979880c3b2362173e6de0283d79708b1c
Nancy <nancy9@sheridancollege.ca>
2024-02-06 12:10:19 -0500

    initial Commit2

--- src/main/java/exercise1/Card.java
+++ src/main/java/exercise1/Card.java
@@ -1,7 +1,7 @@
 package exercise1;
 
-
 /**
+ * 1st in class 
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
  * A suit (clubs, hearts, spades, diamonds).
@@ -12,6 +12,9 @@ package exercise1;
  * @author Paul Bonenfant May 2020
  */
 public class Card {
+
+    public Card(int value1, String suit1) {
+    }
 
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
--- src/main/java/exercise1/CardTrick.java
+++ src/main/java/exercise1/CardTrick.java
@@ -1,67 +1,73 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
+ * It then searches the array of cards for the match to the user's card.
  * To be used as starting code in Exercise
  *
- * @author nancy
- * @author nancy Jan 25, 2022 
+ * @author Nancy
+ * @date Feb 5, 2024
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
+
         Card[] hand = new Card[7];
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            int value = random.nextInt(13) + 1; // Values 1-13 (1=Ace, 11=Jack, 12=Queen, 13=King)
+            String suit = Card.SUITS[random.nextInt(4)]; // Assuming Card.SUITS is a String[] {"Hearts", "Diamonds", "Clubs", "Spades"}
+            hand[i] = new Card(value, suit);
         }
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+        // Ask the user for Card value and suit, create their card
+        System.out.println("Pick a card value (1=Ace, 11=Jack, 12=Queen, 13=King):");
+        int userValue = scanner.nextInt();
+        System.out.println("Pick a suit (0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades):");
+        int suitIndex = scanner.nextInt();
+        String userSuit = Card.SUITS[suitIndex];
+
+        // Search the hand for the user's card
+        boolean found = false;
+        for (Card card : hand) {
+            if (card.getValue() == userValue && card.getSuit().equalsIgnoreCase(userSuit)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, your card was not in the hand.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 
     /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * A simple method to print out personal information.
      */
     private static void printInfo() {
-    
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Nancy, but you can call me dear, Nancy, or sir");
         System.out.println();
-        
-        System.out.println("My career ambitions:");
+        System.out.println("My career ambitions: Software Developer");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
+        System.out.println();
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
-
         System.out.println();
-        
-    
     }
-
 }
